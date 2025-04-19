617352c8c161fe31fe3daee23e61ae9cdd94519c
Bradley Co <bradco626@gmail.com>
2025-04-19 15:16:39 -0700

    GOD FILE STRUCTURE setup

--- frontend/App.js
+++ frontend/App.js
@@ -4,7 +4,8 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>RICK IS Ae BITCH
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
